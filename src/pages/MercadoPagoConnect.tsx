348c669ee849ca804fd3b44c3d72535c7707cd4c
JoseDaniloS <danilojose.1d@gmail.com>
2026-02-15 14:34:24 -0300

    feat: mercado pago redirect uri

--- src/pages/MercadoPagoConnect.tsx
+++ src/pages/MercadoPagoConnect.tsx
@@ -5,7 +5,7 @@ export default function MercadoPagoConnect() {
     const [searchParams] = useSearchParams()
     const code = searchParams.get("code");
     const CLIENT_ID = "1549445475571223"
-    const redirectUri = "https://192.168.1.36:5173/oauth/mercadopago"
+    const redirectUri = window.location.origin + window.location.pathname
 
     useEffect(() => {
         window.location.href = `https://auth.mercadopago.com.br/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}`
