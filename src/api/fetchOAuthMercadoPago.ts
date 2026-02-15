14d799e92a99fce5a350ed989437a911cd0db299
JoseDaniloS <danilojose.1d@gmail.com>
2026-02-15 15:02:21 -0300

    refact: code fetch to api

---
+++ src/api/fetchOAuthMercadoPago.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_URL = "https://clunroropj.execute-api.us-east-1.amazonaws.com/"
+
+export async function fetchOAuthMercadoPago(token: string) {
+    if (!token) {
+        throw new Error("Token não encontrado");
+    }
+    try {
+        const response = await axios.post(`${API_URL}oauth?code=${token}`);
+        return response
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
--- src/pages/MercadoPagoConnect.tsx
+++ src/pages/MercadoPagoConnect.tsx
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
 import { useParams, useSearchParams } from "react-router-dom"
+import { fetchOAuthMercadoPago } from "../api/fetchOAuthMercadoPago";
 
 export default function MercadoPagoConnect() {
     const [searchParams] = useSearchParams()
-    const code = searchParams.get("code");
+    const code = searchParams.get("code") || "";
     const CLIENT_ID = "1549445475571223"
     const redirectUri = window.location.origin + "/oauth/mercadopago"
-    console.log(redirectUri)
+
+    if (!code) {
+        useEffect(() => {
+            window.location.href = `https://auth.mercadopago.com.br/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}`
+        }, [])
+    }
 
     useEffect(() => {
-        window.location.href = `https://auth.mercadopago.com.br/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}`
+        const sendCode = async () => {
+            try {
+                const response = await fetchOAuthMercadoPago(code)
+                console.log(response)
+            } catch (error) {
+
+            }
+        }
+        sendCode()
     }, [])
 
 }
